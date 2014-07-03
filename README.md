f6c96b04485ae23a9af176531a534dc26810301c
Thirasan B <thirasan@internetcenter.co.th>
2014-07-04 05:54:47 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,112 @@
+insdseq-filter
+==============
+This is a Node command line utility for extracting gene information from an INSDSeq XML file.
+The files used for the development of this tool were obtained from http://www.ncbi.nlm.nih.gov/nuccore
+though any source should work as long as they follow the INSDSeq XML specification.
+
+Installation
+------------
+```shell
+git clone https://github.com/mizstik/insdseq-filter.git
+npm install
+```
+
+This package requires xml-stream, which in turn requires node-expat, which requires compiling.
+Therefore, you may run into problems related to compiling the modules during the installation.
+See [this](https://github.com/TooTallNate/node-gyp#installation) page for a list of required components.
+
+On Windows, it can be helpful to specify the version of Visual Studio that you have installed:
+
+```shell
+npm install --msvs_version=2012
+```
+
+This can be quite difficult on Windows, so as a last resort you may try downloading a zipped node_modules folder [here](http://mizstik.github.io/insdseq-filter.node_modules.zip).
+(Windows 64-bit only)
+
+Usage
+-----
+The program parses the INSDSeq XML input file, examining each locus for features that match specified gene or product names.
+When a matching feature is found, it extracts the gene sequence as indicated by the position info and output this to a file.
+Supported output formats are FASTA and CSV.
+
+Running the tool without any argument will display a help text.
+
+```shell
+node insdseq-filter.js
+```
+
+At minimum, you need to specify an input file, which is the INSDSeq XML you want to parse.
+
+```shell
+node insdseq-filter.js --infile=input.xml
+```
+
+By default, the program will only look at CDS features and does not filter them for any gene or product name, outputting all
+found CDS features to the output file which is output.fas by default.
+
+To specify a different output file, use --outfile and --format:
+
+```shell
+node insdseq-filter.js --infile=input.xml --outfile=coii --format=csv
+```
+
+The extension of the output filename is determined by the format. In this example, the output file will be coii.csv.
+Only 'fas' and 'csv' are supported.
+
+To filter for a particular gene or product, use the --filter option:
+
+```shell
+node insdseq-filter.js --infile=input.xml --filter=coii
+```
+
+The above example writes down CDS features with /gene=coii or /product=coii in them. Note that the search is case-insensitive.
+
+You can specify multiple names separated by comma:
+
+```shell
+node insdseq-filter.js --infile=input.xml --filter=coii,cox2,co2
+```
+
+Sometimes, gene or product names can have multiple dozens of synonyms. You can create a text file containing a list of
+the synonyms (separated by newlines instead of comma) and specify this file with --filterfile option:
+
+```shell
+node insdseq-filter.js --infile=input.xml --filterfile=filter.txt
+```
+
+See examples folder for an example of the filter file. Note that due to differences among text editors and the OSes,
+it is recommended that you leave an empty line at the beginning and at the end of the file. Empty lines are ignored.
+
+To search on features other than CDS, use the --feature option. This is also case-insensitive and can be provided
+in a list of comma-separated values:
+
+```shell
+node insdseq-filter.js --infile=input.xml --feature=tRNA,gap
+```
+
+Or use 'all' to run on all features:
+
+```shell
+node insdseq-filter.js --infile=input.xml --feature=all
+```
+
+An example using multiple options:
+
+```shell
+node insdseq-filter.js --infile=input.xml --feature=CDS --filter=coii,cox2,co2 --outfile=coii --format=fas
+```
+
+
+Misc
+----
+You can contact me on [github](https://github.com/Mizstik) or [twitter](https://twitter.com/Mizstik).
+
+This tool was created for a friend of mine for his research project. It seems like this is
+something someone somewhere out there might find useful, so I'm uploading it here.
+
+
+License
+-------
+Copyright 2014 Thirasan Borisuthipandit
+Licensed under the MIT License. (see license.txt)
---
+++ insdseq-filter.js
@@ -0,0 +1,166 @@
+"use strict";
+
+var t_app = process.hrtime();
+ 
+var    	   fs = require('fs'),
+			_ = require('underscore'),
+		 util = require('util'),
+	xmlstream = require('xml-stream');
+		 
+var opt = require('minimist')(process.argv.slice(2));
+
+function chunkify (instring, chunklen) {
+	if (typeof instring != 'string') return false;
+	if (typeof chunklen == 'undefined') chunklen = 70;
+	var output = [];
+	while (instring) {
+		if (instring.length < chunklen) {
+			output.push(instring);
+			break;
+		} else {
+			output.push(instring.substr(0, chunklen));
+			instring = instring.substr(chunklen);
+		}
+	}
+	return output;
+}
+
+var defaultarg = {
+	infile : '',
+	feature : 'CDS', // comma-separated, or 'all'
+	// filter : 'coii,cox2', // comma-separated
+	// filterfile : 'filter.txt',
+	outfile : 'output',
+	format : 'fas' // or csv
+};
+_.defaults(opt, defaultarg);
+
+if (opt.infile == '') {
+	console.log("\n"+"INSDSEQ XML Gene Filter by Thirasan Borisuthipandit, \u00A9 2014 MIT LICENSE (see license.txt)");
+	console.log("\n"+"BASIC USAGE: node insdseq-gene-filter.js --infile=filename.xml --filter=genename\n");
+	console.log("Options:");
+	console.log("\t--infile\tfilename of the input XML\n");
+	console.log("\t--feature\tINSDFeature to filter, default --feature=CDS\n");
+	console.log("\t--filter\tcomma-separated list of genes, e.g. --filter=coii,cox2");
+	console.log("\t\t\tIf filter is not specified, the script will return all genes.\n");
+	console.log("\t--filterfile\tfilename containing filters, e.g. --filter=filter.txt\n");
+	console.log("\t--outfile\tname of output file (extension determined by format)");
+	console.log("\t\t\tdefault --outfile=output");
+	console.log("\t--format\tfas or csv, default --format=fas\n");
+	process.exit(0);
+}
+
+if (opt.format != 'fas' && opt.format != 'csv') {
+	console.log('Invalid output format. (support fas or csv)');
+	process.exit(1);
+}
+
+var no_filter = false;
+var filters = [];
+if (opt.filter) {
+	filters = opt.filter.split(',');
+} else if (opt.filterfile) {
+	var filterraw = fs.readFileSync(opt.filterfile, {encoding:'utf8'});
+	filters = filterraw.split("\r\n");
+	filters = filters.filter(function(n){ return n != '' });
+} else {
+	no_filter = true;
+}
+filters = _.map(filters, function (el) {return el.toUpperCase();});
+
+var no_feature = false;
+if (opt.feature == 'all' || opt.feature == '') {
+	no_feature = true;
+} else {
+	opt.feature = opt.feature.split(',');
+	opt.feature = _.map(opt.feature, function (el) {return el.toUpperCase();});
+}
+
+
+var done = false;
+var locus_count = 0;
+var locus_matched = 0;
+
+var outputFilename = opt.outfile + '.' + opt.format;
+var outstream = fs.createWriteStream(outputFilename, {'flags': 'w'});
+
+if (opt.format == 'csv') {
+	outstream.write("sep=\t\n");
+	outstream.write("Locus\tDefinition\tOrganism\tFeature\tGene\tProduct\tFrom\tTo\tLength\tSequence\n");
+}
+
+var instream = fs.createReadStream(opt.infile);
+var xml = new xmlstream(instream);
+xml.collect('INSDFeature');
+xml.collect('INSDQualifier');
+
+xml.on('endElement: INSDSeq', function(seq) {
+	locus_count++;
+	util.print('Processing locus: '+locus_count+', matched features: '+locus_matched+"\u001B[0G");
+	
+	var entry = {};
+	
+	var features = seq["INSDSeq_feature-table"].INSDFeature;
+	
+	_.each(features, function (feature) {
+		if (opt.feature.indexOf(feature.INSDFeature_key.toUpperCase()) > -1 || no_feature) {
+			var gene = '';
+			var product = '';
+			
+			_.each(feature.INSDFeature_quals.INSDQualifier, function (qualifier) {
+				if (qualifier.INSDQualifier_name == 'gene') gene = qualifier.INSDQualifier_value;
+				if (qualifier.INSDQualifier_name == 'product') product = qualifier.INSDQualifier_value;
+			});
+			
+			if ((filters.indexOf(gene.toUpperCase()) > -1) || (filters.indexOf(product.toUpperCase()) > -1) || no_filter) {
+				locus_matched++;
+				
+				entry.locus = seq.INSDSeq_locus;
+				entry.definition = seq.INSDSeq_definition;
+				entry.organism = seq.INSDSeq_organism;
+				entry.feature = feature.INSDFeature_key;
+				entry.from = feature.INSDFeature_intervals.INSDInterval.INSDInterval_from;
+				entry.to = feature.INSDFeature_intervals.INSDInterval.INSDInterval_to;
+				entry.size = entry.to - entry.from + 1;
+				entry.sequence = seq.INSDSeq_sequence.substring(entry.from - 1, entry.to);
+				
+				entry.gene = gene;
+				entry.product = product;
+				
+				if (typeof entry.gene == 'undefined') entry.gene = '';
+				if (typeof entry.product == 'undefined') entry.product = '';
+				
+				if (opt.format == 'csv') {
+					var csv = '"' + entry.locus + '"' + "\t"
+					+ '"' + entry.definition + '"' + "\t"
+					+ '"' + entry.organism + '"' + "\t"
+					+ '"' + entry.feature + '"' + "\t"
+					+ '"' + entry.gene + '"' + "\t"
+					+ '"' + entry.product + '"' + "\t"
+					+ '"' + entry.from + '"' + "\t"
+					+ '"' + entry.to + '"' + "\t"
+					+ '"' + entry.size + '"' + "\t"
+					+ '"' + entry.sequence + '"' + "\n";
+					
+					outstream.write(csv);
+					
+				} else if (opt.format == 'fas') {
+					var fas = '>'+entry.locus+' '+entry.organism+' '+entry.feature+' '+entry.gene+' '+entry.product+"\n";
+					var seqchunk = chunkify(entry.sequence, 70);
+					fas += seqchunk.join("\n") + "\n\n";
+					
+					outstream.write(fas);
+				}
+			}
+		}
+	});
+	
+});
+
+process.on('exit', function () {
+	t_app = process.hrtime(t_app);
+	var timetext = (t_app[0] + t_app[1]/1000000000).toFixed(2) + ' seconds';
+	console.log("\n"+'Time used: '+timetext);
+	console.log('Locus processed: '+locus_count);
+	console.log('Features matched: '+locus_matched);
+});
